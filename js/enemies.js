4165ea868d3e2f637812b554417c52f6543ac6a8
David Peter <david@1000memories.com>
2013-04-28 18:48:24 -0400

    Make more enemies and fix protagonist walk

--- js/enemies.js
+++ js/enemies.js
@@ -1,20 +1,23 @@
+function makeEnemy(x, y, opts) {
+    return Crafty.e('2D, DOM, Enemy, Collision, FollowAI, EnemySprite')
+                .attr({ x: x, y: y-60, w: 54, h: 60,
+                    disabled: false, speed: 2, up: true,
+                    leftLimit: opts.leftLimit || 0,
+                    rightLimit: opts.rightLimit || 0 })
+                .collision()
+                .followAI();
+}
+
 var Enemies = {
     drawEnemies: function() {
-        var enemy1 = Crafty.e('2D, DOM, Enemy, Color, Collision, FollowAI, EnemySprite')
-                        .color('rgb(0, 255, 255)')
-                        .attr({ x: 290, y: 600-60, w: 54, h: 60,
-                            disabled: false, speed: 2, up: true, leftLimit: 0, rightLimit: 290})
-                        .collision()
-                        .followAI();
-
+        var enemy1 = makeEnemy(290, 600, {leftLimit: 0, rightLimit: 290});
         enemies.push(enemy1);
 
-        var enemy2 = Crafty.e('2D, DOM, Enemy, Color, Collision, FollowAI, EnemySprite')
-                        .color('rgb(0, 255, 255)')
-                        .attr({ x: 490, y: 600-60, w: 54, h: 60,
-                            disabled: false, speed: 1, up: false, leftLimit: 352, rightLimit: 490 })
-                        .collision()
-                        .followAI();
+        var enemy2 = makeEnemy(490, 600, {leftLimit: 0, rightLimit: 290});
         enemies.push(enemy2);
+
+        enemies.push(makeEnemy(1800, 600, {leftLimit: 1550, rightLimit: 1900}));
+        enemies.push(makeEnemy(1850, 600, {leftLimit: 1550, rightLimit: 1900}));
+        enemies.push(makeEnemy(1800, 500, {leftLimit: 1600, rightLimit: 1900}));
     }
 };
